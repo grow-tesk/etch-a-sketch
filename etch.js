7fc79f45b787ae6bfdb6e8d8ac732c89eb15eb29
grow-tesk <git config --global user.email zweinman@hotmail.com>
2024-02-07 14:41:55 -0500

    Create 16 divs
    Created a function that creates a certain number of divs with a black
    border for visibility.

--- etch.js
+++ etch.js
@@ -1,7 +1,15 @@
 const grid = document.querySelector('.flexContainer');
 
-const square = document.createElement('div');
-square.style.border = '2px solid black';
-square.classList.add('square');
-
-grid.appendChild(square);
+const createGrid = function () {
+    numberOfSquares = 16;
+    
+    for (x = 0; x <= numberOfSquares; x++) {
+        const oneSquare = document.createElement('div');
+        oneSquare.classList.add('square');
+        oneSquare.style.border = '2px solid black';
+        oneSquare.style.width = '30px';
+        oneSquare.style.height = '30px';
+        grid.appendChild(oneSquare);
+    };
+};
+createGrid();
