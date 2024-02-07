afbe1dd73c2fa770a15be51d4e2d029653944610
grow-tesk <git config --global user.email zweinman@hotmail.com>
2024-02-07 13:52:04 -0500

    Test DOM manipulation
    Making sure DOM connected correctly to HTML, etc.

---
+++ etch.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector('.flexContainer');
+
+const square = document.createElement('div');
+square.style.border = '2px solid black';
+square.classList.add('square');
+
+grid.appendChild(square);
